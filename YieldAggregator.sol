73bab4faf27043ecc1fa44737939368b788775dc
kantorkid <yourgitemail@example.com>
2023-06-02 14:37:19 -0400

    2nd commit

--- YieldAggregator.sol
+++ YieldAggregator.sol
@@ -1,103 +1,134 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.5.16;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+
+interface WrappedETH {
+    function deposit() external payable;
+    function withdraw(uint256) external;
+    function approve(address, uint256) external returns (bool);
+    function transfer(address, uint256) external returns (bool);
+    function transferFrom(address, address, uint256) external returns (bool);
+    function balanceOf(address) external view returns (uint256);
+}
+
+interface CompoundETH {
+    function mint() external payable;
+    function redeem(uint256) external returns (uint256);
+    function supplyRatePerBlock() external view returns (uint256);
+    function balanceOf(address) external view returns (uint256);
+}
+
+interface AaveLendingPool {
+    function deposit(
+        address asset,
+        uint256 amount,
+        address onBehalfOf,
+        uint16 referralCode
+    ) external;
+    function withdraw(
+        address asset,
+        uint256 amount,
+        address to
+    ) external;
+    function getReserveData(address asset) external returns (
+        uint256 configuration,
+        uint128 liquidityIndex,
+        uint128 variableBorrowIndex,
+        uint128 currentLiquidityRate,
+        uint128 currentVariableBorrowRate,
+        uint128 currentStableBorrowRate,
+        uint40 lastUpdateTimestamp,
+        address aTokenAddress,
+        address stableDebtTokenAddress,
+        address variableDebtTokenAddress,
+        address interestRateStrategyAddress,
+        uint8 id
+    );
+}
 
 contract YieldAggregator {
-    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
 
-    address public compoundAddress; // Address of the Compound contract
-    address public aaveAddress; // Address of the Aave contract
+    string public name = "Yield Aggregator";
+    address public owner;
+    address public locationOfFunds;
+    uint256 public amountDeposited;
 
-    IERC20 public wethToken; // ERC20 token used (WETH)
+    WrappedETH private wrappedETH;
+    CompoundETH private compoundETH;
+    AaveLendingPool private aaveLendingPool;
 
-    constructor(address _compoundAddress, address _aaveAddress, address _wethToken) {
-        compoundAddress = _compoundAddress;
-        aaveAddress = _aaveAddress;
-        wethToken = IERC20(_wethToken);
+    event Deposit(address owner, uint256 amount, address depositTo);
+    event Withdraw(address owner, uint256 amount, address withdrawFrom);
+    event Rebalance(address owner, uint256 amount, address depositTo);
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
     }
 
-    function deposit(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than zero");
+    constructor(address wrappedETHAddress, address compoundETHAddress, address aaveLendingPoolAddress) public {
+        owner = msg.sender;
+        wrappedETH = WrappedETH(wrappedETHAddress);
+        compoundETH = CompoundETH(compoundETHAddress);
+        aaveLendingPool = AaveLendingPool(aaveLendingPoolAddress);
+    }
 
-        // Approve transfer of WETH to both Compound and Aave contracts
-        wethToken.safeApprove(compoundAddress, amount);
-        wethToken.safeApprove(aaveAddress, amount);
+    function deposit(uint256 _amount, uint256 _compAPY, uint256 _aaveAPY) public onlyOwner {
+        require(_amount > 0, "Invalid amount");
 
-        // Get the current APY rates of Compound and Aave
-        uint256 compoundAPY = getCompoundAPY();
-        uint256 aaveAPY = getAaveAPY();
+        if (amountDeposited > 0) {
+            rebalance(_compAPY, _aaveAPY);
+        }
 
-        if (compoundAPY >= aaveAPY) {
-            // Deposit WETH into Compound
-            compoundDeposit(amount);
+        wrappedETH.transferFrom(msg.sender, address(this), _amount);
+        wrappedETH.deposit.value(_amount)();
+
+        amountDeposited = amountDeposited.add(_amount);
+
+        if (_compAPY > _aaveAPY) {
+            compoundETH.mint.value(_amount)();
+            locationOfFunds = address(compoundETH);
         } else {
-            // Deposit WETH into Aave
-            aaveDeposit(amount);
+            aaveLendingPool.deposit(address(wrappedETH), _amount, address(this), 0);
+            locationOfFunds = address(aaveLendingPool);
         }
+
+        emit Deposit(owner, _amount, locationOfFunds);
     }
 
-    function rebalance() external {
-        // Get the current APY rates of Compound and Aave
-        uint256 compoundAPY = getCompoundAPY();
-        uint256 aaveAPY = getAaveAPY();
+    function withdraw(uint256 _amount) public onlyOwner {
+        require(_amount > 0 && _amount <= amountDeposited, "Invalid amount");
 
-        if (compoundAPY > aaveAPY) {
-            // Withdraw from Aave and deposit into Compound
-            uint256 aaveBalance = getAaveBalance();
-            aaveWithdraw(aaveBalance);
-            compoundDeposit(aaveBalance);
-        } else if (aaveAPY > compoundAPY) {
-            // Withdraw from Compound and deposit into Aave
-            uint256 compoundBalance = getCompoundBalance();
-            compoundWithdraw(compoundBalance);
-            aaveDeposit(compoundBalance);
-        }
-    }
-
-    function withdraw(uint256 amount) external {
-        require(amount > 0, "Amount must be greater than zero");
-
-        uint256 compoundBalance = getCompoundBalance();
-        uint256 aaveBalance = getAaveBalance();
-
-        if (amount <= compoundBalance) {
-            // Withdraw from Compound
-            compoundWithdraw(amount);
-        } else if (amount <= compoundBalance + aaveBalance) {
-            // Withdraw from Aave
-            aaveWithdraw(amount - compoundBalance);
+        if (locationOfFunds == address(compoundETH)) {
+            uint256 compoundETHBalance = compoundETH.balanceOf(address(this));
+            uint256 redeemAmount = compoundETHBalance.mul(_amount).div(amountDeposited);
+            uint256 withdrawnAmount = compoundETH.redeem(redeemAmount);
+            require(withdrawnAmount > 0, "Compound withdraw failed");
+            require(address(this).balance >= withdrawnAmount, "Insufficient ETH balance");
+            msg.sender.transfer(withdrawnAmount);
         } else {
-            revert("Insufficient funds");
+            aaveLendingPool.withdraw(address(wrappedETH), _amount, msg.sender);
+        }
+
+        amountDeposited = amountDeposited.sub(_amount);
+
+        emit Withdraw(owner, _amount, locationOfFunds);
+    }
+
+    function rebalance(uint256 _compAPY, uint256 _aaveAPY) internal {
+        if (locationOfFunds == address(compoundETH)) {
+            uint256 compoundETHBalance = compoundETH.balanceOf(address(this));
+            uint256 redeemAmount = compoundETHBalance.mul(amountDeposited).div(compoundETHBalance.add(amountDeposited));
+            uint256 redeemedAmount = compoundETH.redeem(redeemAmount);
+            require(redeemedAmount > 0, "Compound redeem failed");
+            require(address(this).balance >= redeemedAmount, "Insufficient ETH balance");
+            wrappedETH.withdraw(redeemedAmount);
+        } else {
+            aaveLendingPool.withdraw(address(wrappedETH), amountDeposited, address(this));
         }
     }
 
-    // Helper functions for interacting with Compound and Aave contracts
-    function compoundDeposit(uint256 amount) internal {
-        // Implement the deposit logic for Compound
-        // ...
-    }
-
-    function compoundWithdraw(uint256 amount) internal {
-        // Implement the withdrawal logic for Compound
-        // ...
-    }
-
-    function aaveDeposit(uint256 amount) internal {
-        // Implement the deposit logic for Aave
-        // ...
-    }
-
-    function aaveWithdraw(uint256 amount) internal {
-        // Implement the withdrawal logic for Aave
-        // ...
-    }
-
-    // Helper functions to get APY rates
-    function getCompoundAPY() internal view returns (uint256) {
-        // Implement the logic to get the APY rate from Compound
-        // ...
-    }
-
-    function getAaveAP
+    function() external payable {}
+}
